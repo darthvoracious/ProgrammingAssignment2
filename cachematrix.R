16c8544eeaa98354f4891fd210282402e79441c7
unknown <cviveiros@CVIVEIROS-PC.osprey.com>
2015-08-22 16:55:02 -0400

    Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inverse = NULL
+  set = function (value) {
+    x <<- value
+    inverse <<- NULL
+  }
+  
+  get = function() x
+  setinverse = function(value) inverse <<- value
+  getinverse = function() inverse
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 
 }
 
@@ -12,4 +23,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+        inverse = x$getinverse()
+        
+        if (!is.null(inverse)){
+          # get it from the cache
+          message("Get Cached Data")
+          return(inverse)
+        }
+        
+        mat.data = x$get()
+        inverse = solve(mat.data, ...)
+        
+        x$setinverse(inverse)
+        
+        return(inverse)
 }
---
+++ matrixTest.R
@@ -0,0 +1,16 @@
+source("C:/Users/cviveiros/Google Drive/Projects/R/Week3/Assignmnent2/cachematrix.r")
+test = function(mat){
+  ## @mat: an invertible matrix
+  
+  temp = makeCacheMatrix(mat)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+  
+  start.time = Sys.time()
+  cacheSolve(temp)
+  dur = Sys.time() - start.time
+  print(dur)
+}
